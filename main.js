1845557a41a0d4c09eff7c825a6005a8cddaca22
Alex43961 <semen43961@gmail.com>
2023-05-03 20:08:10 +0300

    finished

--- main.js
+++ main.js
@@ -3,27 +3,22 @@
 
 //!------------------------- -2 ----------------------------
 
-const newArray = function(firstNumber, lengthOfArray){
+const newArray = function (firstNumber, lengthOfArray) {
 	let myArray = [];
-	for(let i = firstNumber; i< (lengthOfArray + firstNumber); i++){
+	for (let i = firstNumber; i < (lengthOfArray + firstNumber); i++) {
 		myArray.push(i);
 	}
 	return myArray;
 }
 
-console.log(newArray(6, 15));
-function myArray(firstNumber, lengthOfArray){
-	let secondArray = [];
-	let i = firstNumber;
-	while(i < (lengthOfArray + firstNumber)){
-		secondArray.push(i);
-		i++;
-	}
-	return secondArray;
+console.log(newArray(15, 8));
+
+function array3 (firstNumber, lengthOfArray) {
+	let x = Array.from({ length: (lengthOfArray ) }, (_, i) => i + firstNumber);
+	return x;
 }
-console.log(myArray(9, 5));
 
-
+console.log(array3(5, 5));
 
 //! -------------------------0 -----------------------------
 
@@ -33,16 +28,16 @@ function borderColor() {
 	let note = textarea.value;
 
 
-button.onclick = () => {
-	let input = document.getElementById("input").value;
-	const regExp = new RegExp(`${input}`, "i")
-	console.log(regExp);
-	if(!regExp.test(note) || `${input}` === "") {
-		textarea.style.borderColor = "orange";
-	} else {
-		textarea.style.borderColor = "lime";
-	}
-};
+	button.onclick = () => {
+		let input = document.getElementById("input").value;
+		const regExp = new RegExp(`${input}`, "i")
+
+		if (!regExp.test(note) || `${input}` === "") {
+			textarea.style.borderColor = "orange";
+		} else {
+			textarea.style.borderColor = "lime";
+		}
+	};
 }
 borderColor();
 
@@ -50,20 +45,52 @@ borderColor();
 
 //!---------------------------1-------------------------------
 
-const doubleCharacter = function() {
+const doubleCharacter = function () {
 	let doubleButton = document.querySelector("#doubleButton");
 }
 doubleButton.onclick = () => {
 	let textarea = document.querySelector("#textarea2");
 	let textareaValue = document.querySelector("#textarea2").value;
-	console.log(textareaValue);	
-	
+	console.log(textareaValue);
+
 	let newText = textareaValue.replace(/(\p{Emoji}[^\d+])/gu, "$1$1");
 	console.log(newText);
 	textarea.value = newText;
 };
 
 doubleCharacter();
+
+//!---------------------------- 2 -----------------------------
+
+function replaceText() {
+	let ex3Wrapper = document.querySelector(".ex3_wrapper")
+	let textarea = document.querySelector("#textarea3");	
+	const plusButton = document.querySelector("#plusButton");
+	const replaceButton = document.querySelector("#replaceButton");
+
+	plusButton.addEventListener("click", () => {
+		let input = document.createElement('input');
+		input.classList.add("input3");
+		input.setAttribute("type", "text");
+		input.setAttribute("placeholder", "Enter text");
+
+
+		input.addEventListener("dblclick", () => {
+			input.remove();
+		});
+		ex3Wrapper.append(input);
+	});
+
+	replaceButton.addEventListener("click", () => {
+		let input = document.getElementsByClassName("input3");
+		for (let i = 0; i < input.length; i++) {
+			let regExp = new RegExp(input[i].value, "gi");
+			textarea.value = textarea.value.replace(regExp, "*".repeat(input[i].value.length));
+		}
+	});
+}
+
+replaceText();
 
 
 
